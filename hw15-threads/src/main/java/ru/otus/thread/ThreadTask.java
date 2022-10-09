44a30099bd76ac5df287107f2b1324fb493808cd
druespin <wasisdust@gmail.com>
2022-10-09 17:41:29 +0300

    hw15-threads review

--- hw15-threads/src/main/java/ru/otus/thread/ThreadTask.java
+++ hw15-threads/src/main/java/ru/otus/thread/ThreadTask.java
@@ -1,60 +1,65 @@
 package ru.otus.thread;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 
 public class ThreadTask {
 
     private static final Logger logger = LoggerFactory.getLogger(ThreadTask.class);
 
-    private static final Object monitor = new Object();
-
     public static void main(String[] args) {
+        var task = new ThreadTask();
+        task.go();
+    }
 
-        var t1 = new NewThread("t1", monitor);
-        var t2 = new NewThread("t2", monitor);
+    private void go() {
+        var t1 = new NewThread("t1");
+        var t2 = new NewThread("t2");
 
         t1.setPriority(Thread.MAX_PRIORITY);
+
         t1.start();
         t2.start();
     }
 
+    private synchronized void printSequences(String name) {
+        for (int i = 1; i < 10; i++) {
+            workInThread(name, i);
+        }
+        for (int i = 10; i > 1; i--) {
+            workInThread(name, i);
+        }
+    }
 
-    static class NewThread extends Thread {
+    private void workInThread(String name, int i) {
+        Thread.interrupted();
+        try {
+            this.notify();
+            logger.info(name + "-" + i);
+            this.wait();
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            // в случае прерывания текущего потока другим потоком путем notify флаг interrupted не переключается
+            if (!Thread.interrupted()) {
+                logger.error("Spurious wakeup");
+            }
+            Thread.currentThread().interrupt();
+        }
+    }
 
-        private final Object monitor;
+    class NewThread extends Thread {
+
         private final String name;
 
-        public NewThread(String name, Object monitor) {
+        public NewThread(String name) {
             this.name = name;
-            this.monitor = monitor;
         }
 
         @Override
         public void run() {
-            try {
-                while (true) {
-                    for (int i = 1; i < 10; i++) {
-                        synchronized (monitor) {
-                            monitor.notify();
-                            logger.info(name + "-" + i);
-                            monitor.wait();
-                            Thread.sleep(500);
-                        }
-                    }
-                    for (int i = 10; i > 1; i--) {
-                        synchronized (monitor) {
-                            monitor.notify();
-                            logger.info(name + "-" + i);
-                            monitor.wait();
-                            Thread.sleep(500);
-                        }
-                    }
-                }
-            }
-            catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
+            while (true) {
+                printSequences(name);
             }
         }
     }
